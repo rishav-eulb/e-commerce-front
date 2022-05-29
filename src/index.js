ace6c77b9fffb765e975a5611c7d049b22f260ed
rishav-eulb <77015951+rishav-eulb@users.noreply.github.com>
2022-05-29 13:47:33 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
@@ -11,17 +11,18 @@ import { Provider } from "react-redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "./reducers";
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 // store
 const store = createStore(rootReducer, composeWithDevTools());
 
-ReactDOM.render(
+root.render(
   // <React.StrictMode>
   <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </Provider>,
-  // </React.StrictMode>,
-  document.getElementById("root")
+  // </React.StrictMode>
+  
 );
 
